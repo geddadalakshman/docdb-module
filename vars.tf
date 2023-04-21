8e5dc295ed65e60bd9c63f272e1fe60888ac6a92
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-21 22:33:52 +0530

    updated

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "engine" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "tags" {}
+variable "subnet_ids" {}
+
