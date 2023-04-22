22cc347de1f793f446ce340e8dfb882c31a79a1e
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-04-22 15:34:53 +0530

    updated

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_ssm_parameter" "user" {
-  name = "${var.env}-docdb.user"
+  name = "${var.env}.docdb.user"
 }
 
 data "aws_ssm_parameter" "pass" {
-  name = "${var.env}-docdb.pass"
+  name = "${var.env}.docdb.pass"
 }
